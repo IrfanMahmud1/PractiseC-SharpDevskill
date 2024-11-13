04793ebd525828f1de77220a6de082cafaedc3c5
IrfanMahmud <irfanmahmud201@gmail.com>
2024-11-13 08:30:43 +0600

    generics with bubble sort example

---
+++ Generics Example/Bubble Sort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Example
+{
+    public class BubbleSort<T> where T : INumber<T>
+    {
+        public T[] Sort(params T[] num)
+        {
+            var temp = default(T);
+
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                for(int j = 0; j < num.Length - 1;  j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        temp = num[j + 1];
+                        num[j + 1] = num[j];
+                        num[j] = temp;
+                    }
+                }
+            }
+
+            return num;
+        }
+
+    }
+}
--- Generics Example/Program.cs
+++ Generics Example/Program.cs
@@ -4,3 +4,19 @@ Coordinate<int,double> coordinate = new Coordinate<int,double>();
 
 coordinate.X = 5;
 coordinate.Y = 6.5;
+
+BubbleSort<int> bubbleSort = new BubbleSort<int>();
+var result = bubbleSort.Sort(7, 2, 1, 9, 4, 8, 3);
+
+foreach (var item in result)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+BubbleSort<double> bubbleSort2 = new BubbleSort<double>();
+var result2 = bubbleSort2.Sort(7.2, 2.3, 1.5, 9.2, 4.5, 8.1, 3.6);
+
+foreach (var item in result2)
+{
+    Console.Write(item + " ");
+}
