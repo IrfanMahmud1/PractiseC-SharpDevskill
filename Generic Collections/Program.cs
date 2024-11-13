066e147aad15371fe0c5d35b96af84245996352d
IrfanMahmud <irfanmahmud201@gmail.com>
2024-11-13 12:20:23 +0600

    dicitonary

--- Generic Collections/Program.cs
+++ Generic Collections/Program.cs
@@ -9,3 +9,14 @@ foreach (var item in list)
 {
     Console.WriteLine(item);
 }
+
+Dictionary<string, int> dictionary = new Dictionary<string, int>();
+dictionary.Add("Irfan", 25);
+dictionary.Add("Mahmud", 24);
+dictionary.Add("Ashik", 26);
+dictionary["Ashik"] = 22;
+foreach (var item in dictionary)
+{
+    Console.WriteLine($"key : {item.Key} Value: {item.Value}");
+}
+
