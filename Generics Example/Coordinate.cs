95545a8e17c7137235db64fbaad20ea40e5242a6
IrfanMahmud <irfanmahmud201@gmail.com>
2024-11-13 07:54:12 +0600

    basic concept of generics with an example

---
+++ Generics Example/Coordinate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Example
+{
+    internal class Coordinate<T,M>
+    {
+        public T X {  get; set; }
+        public M Y {  get; set; }
+    }
+}
---
+++ Generics Example/Program.cs
@@ -0,0 +1,6 @@
+﻿using Generics_Example;
+
+Coordinate<int,double> coordinate = new Coordinate<int,double>();
+
+coordinate.X = 5;
+coordinate.Y = 6.5;
