0ca9359a00fd71836d7bba72525f11c7fc361983
IrfanMahmud <irfanmahmud201@gmail.com>
2024-11-25 19:11:46 +0600

    added 5 solutions

--- CFProblemSolve/Program.cs
+++ CFProblemSolve/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿var input1 = Console.ReadLine().Split(' ');
+var input2 = Console.ReadLine().Split(' ');
+
+var n = int.Parse(input1[0]);
+var t = int.Parse(input1[1]);
+var time = t;
+var mx = 0;
+var ans = 0;
+if(n == 1 && int.Parse(input2[0]) <= time)
+{
+    Console.WriteLine(1);
+    return;
+}
+for (int i = 0; i < n - 1; i++)
+{
+    var a = int.Parse(input2[i]);
+    var b = int.Parse(input2[i + 1]);
+    
+    if (a <= time)
+    {
+        mx++;
+        time -= a;
+        if (i == n - 2 && b <= time)
+        {
+            mx++;
+            ans = Math.Max(ans, mx);
+        }
+        if (b > time)
+        {
+            ans = Math.Max(ans, mx);
+            if(a + b <= t)
+            {
+                mx = 1;
+            }
+            else
+            {
+                mx = 0;
+            }
+            time = t;
+        }
+    }
+}
+Console.WriteLine(ans);
